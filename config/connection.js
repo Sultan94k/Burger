8f021c3ef78238bc661b2f0d91dde894d380d774
Sultan Karimov <skarimovkgz@gmail.com>
2019-10-27 23:53:03 -0500

    pushing with no style

--- config/connection.js
+++ config/connection.js
@@ -1,39 +1,37 @@
-//todo connect the the databse
-
-// Require mysql npm package to create a connection to the mysql database.
+//Require mysql npm package to create a connection to the mysql database.
 var mysql = require("mysql");
 
-// Read and set any enviroment variables with the dot env package.
-require('dotenv').config();
+//Read and set any environment variables with the dotenv package.
+require("dotenv").config();
 
-//define database connection properties (host, user, password, and database name)
-//use prodoction database when daployed.
-
+//Define database connection properties (host, user, password, and database name)
+//Use production database when deployed.
 if (process.env.JAWSDB_URL) {
-    //This is Heroku deployment
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  //Heroku deployment
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 
 else {
-    //else use local host database for local development.
-    //Mysql password passed into connection.js from  the .env file using the dotenv package.
-    var connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "asdfghj89",
-        database: "burger_db"
-    });
-};
+  //else use localhost database for local development.
+  //MySQL password is passed into connection.js from the .env file using the dotenv npm package.
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "asdfghj89",
+    database: "burger_db"
+  });
+}
 
-connection.connect(function (err) {
-    //if ther is an error connecting to the database, log the error to the console.
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    //if database connection is established, log the database thread number.
-    console.log("connected as id " + connection.threadId);
+connection.connect(function(err) {
+  //If there is an error when connecting to the database, log the error to the console.
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  //If a database connection is established, log the database thread number.
+  console.log("connected as id " + connection.threadId);
 });
 
+//Export the connection properties so that we can use them in other files.
 module.exports = connection;
